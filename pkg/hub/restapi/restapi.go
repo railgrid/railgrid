faroshq/faros

feat: suggest users by email prefix in the add-member box
GET /api/users/search?q=<prefix> suggests accounts whose email or display
name starts with the query, for the portal's add-member input.

It lets any signed-in person discover other accounts' emails, so it is
narrow on purpose:
- signed-in users only; delegated provider tokens get 403
- q must be at least 5 characters; prefix match only, at most 5 results
- per-user token bucket: burst of 10, then one request every 6 seconds,
  429 with Retry-After past that; malformed queries are charged too
- accounts without an email (static tokens), accounts being deleted and
  the caller are never suggested

The portal debounces, reuses a complete answer for a prefix to filter
longer queries locally, and stops asking until Retry-After after a 429,
so typing one address usually costs one request. The add-member input is
a combobox (arrow keys, Enter, Escape) and skips existing members.

Also drops the "others add you with it" hint from the account menu.

pkg/hub/restapi/restapi.go
@@ -56,6 +56,7 @@ import (
 	"github.com/faroshq/faros/pkg/hub/kcp"
 	"github.com/faroshq/faros/pkg/hub/providers"
 	"github.com/faroshq/faros/pkg/hub/tenant"
+	"github.com/faroshq/faros/pkg/server/proxy"
 )
 
 // WorkspaceOps is the slice of *kcp.Bootstrapper the REST handlers
@@ -264,10 +265,14 @@ func (m *Manager) WithOrgProviders(ops OrgProviderOps, creds ProviderCredentialM
 // /api/* endpoints across the two middlewares.
 type Handler struct {
 	mgr *Manager
+	// userSearch rate-limits GET /api/users/search per caller.
+	userSearch *proxy.IPRateLimiter
 }
 
 // NewHandler constructs a Handler.
-func NewHandler(mgr *Manager) *Handler { return &Handler{mgr: mgr} }
+func NewHandler(mgr *Manager) *Handler {
+	return &Handler{mgr: mgr, userSearch: newUserSearchLimiter()}
+}
 
 // RegisterUserOnly attaches the routes that only need the caller's
 // User identity (no active Org / Workspace yet). r is the subrouter
@@ -278,12 +283,14 @@ func NewHandler(mgr *Manager) *Handler { return &Handler{mgr: mgr} }
 //	GET    /api/orgs                       list orgs the caller is in
 //	POST   /api/orgs                       create a new Org
 //	GET    /api/users/me                   the caller's own identity
+//	GET    /api/users/search?q=            suggest users by email/name prefix (rate-limited)
 //	DELETE /api/users/me                   soft-delete self (O-8)
 //	POST   /api/users/me/undelete          undelete self (O-8)
 func (h *Handler) RegisterUserOnly(r *mux.Router) {
 	r.HandleFunc("/orgs", h.listOrgs).Methods(http.MethodGet)
 	r.HandleFunc("/orgs", h.createOrg).Methods(http.MethodPost)
 	r.HandleFunc("/users/me", h.getSelfUser).Methods(http.MethodGet)
+	r.HandleFunc("/users/search", h.searchUsers).Methods(http.MethodGet)
 	r.HandleFunc("/users/me", h.deleteSelfUser).Methods(http.MethodDelete)
 	r.HandleFunc("/users/me/undelete", h.undeleteSelfUser).Methods(http.MethodPost)
 }

pkg/hub/restapi/user_search_test.go
@@ -1,0 +1,178 @@
+/*
+Copyright 2026 The Faros Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package restapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+
+	"github.com/gorilla/mux"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	tenancyv1alpha1 "github.com/faroshq/faros/apis/tenancy/v1alpha1"
+	"github.com/faroshq/faros/pkg/hub/tenant"
+)
+
+func searchTestUser(name, email, displayName string) *tenancyv1alpha1.User {
+	return &tenancyv1alpha1.User{
+		ObjectMeta: metav1.ObjectMeta{Name: name},
+		Spec:       tenancyv1alpha1.UserSpec{Email: email, Name: displayName, RBACIdentity: "faros:" + email},
+	}
+}
+
+func getUserSearch(t *testing.T, srv *httptest.Server, q string) (int, http.Header, []UserSuggestion) {
+	t.Helper()
+	resp, err := http.Get(srv.URL + "/api/users/search?q=" + url.QueryEscape(q))
+	if err != nil {
+		t.Fatalf("GET /api/users/search: %v", err)
+	}
+	defer func() { _ = resp.Body.Close() }()
+	var body ListResponse[UserSuggestion]
+	if resp.StatusCode == http.StatusOK {
+		if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
+			t.Fatalf("decoding: %v", err)
+		}
+	}
+	return resp.StatusCode, resp.Header, body.Items
+}
+
+func TestSearchUsers(t *testing.T) {
+	deleting := searchTestUser("user-gone", "carol.gone@example.com", "Carol Gone")
+	now := metav1.Now()
+	deleting.Status.DeletionRequestedAt = &now
+	static := &tenancyv1alpha1.User{
+		ObjectMeta: metav1.ObjectMeta{Name: "static-user-47b9dce0e91570a1"},
+		Spec:       tenancyv1alpha1.UserSpec{Name: "faros:static:47b9dce0e91570a1", RBACIdentity: "faros:static:47b9dce0e91570a1"},
+	}
+	mgr, _, _ := newTestManager(t,
+		searchTestUser("user-alice", "alice@example.com", "Alice"),
+		searchTestUser("user-carol", "Carol.Smith@example.com", "Carol Smith"),
+		searchTestUser("user-carl", "carl@example.com", "Carolina Jones"),
+		deleting,
+		static,
+	)
+	srv := newTestServer(t, mgr, adminTC("user-alice", "", ""))
+	defer srv.Close()
+
+	for _, tc := range []struct {
+		name string
+		q    string
+		want []string
+	}{
+		// Case-insensitive email prefix; a soft-deleted account is left out.
+		{name: "email prefix", q: "CAROL.", want: []string{"user-carol"}},
+		// Email and display-name matches combine, sorted by email.
+		{name: "email or name prefix", q: "carol", want: []string{"user-carl", "user-carol"}},
+		// Display-name prefix matches too.
+		{name: "name prefix", q: "carolina", want: []string{"user-carl"}},
+		// The caller is never suggested to themselves.
+		{name: "self excluded", q: "alice@", want: nil},
+		// Static-token users have no email and are never suggested, even
+		// though their display names share this prefix.
+		{name: "static excluded", q: "faros:static:", want: nil},
+		// Prefix only, not substring.
+		{name: "no substring match", q: "example.com", want: nil},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			code, _, items := getUserSearch(t, srv, tc.q)
+			if code != http.StatusOK {
+				t.Fatalf("status %d, want 200", code)
+			}
+			var got []string
+			for _, s := range items {
+				got = append(got, s.User)
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("q=%q suggested %v, want %v", tc.q, got, tc.want)
+			}
+		})
+	}
+
+	if code, _, _ := getUserSearch(t, srv, "caro"); code != http.StatusBadRequest {
+		t.Errorf("4-character query: status %d, want 400", code)
+	}
+}
+
+func TestSearchUsersCapsResults(t *testing.T) {
+	var objs []runtime.Object
+	for i := 7; i >= 0; i-- {
+		objs = append(objs, searchTestUser(fmt.Sprintf("user-%d", i), fmt.Sprintf("teammate%d@example.com", i), ""))
+	}
+	mgr, _, _ := newTestManager(t, objs...)
+	srv := newTestServer(t, mgr, adminTC("someone", "", ""))
+	defer srv.Close()
+
+	code, _, items := getUserSearch(t, srv, "teammate")
+	if code != http.StatusOK {
+		t.Fatalf("status %d, want 200", code)
+	}
+	var got []string
+	for _, s := range items {
+		got = append(got, s.Email)
+	}
+	want := []string{"teammate0@example.com", "teammate1@example.com", "teammate2@example.com", "teammate3@example.com", "teammate4@example.com"}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %v, want the first %d by email", got, UserSearchMaxResults)
+	}
+}
+
+func TestSearchUsersRateLimited(t *testing.T) {
+	mgr, _, _ := newTestManager(t, searchTestUser("user-bob", "bob@example.com", "Bob"))
+	srv := newTestServer(t, mgr, adminTC("user-alice", "", ""))
+	defer srv.Close()
+
+	// Invalid queries are charged too, so probing costs the same.
+	for i := range userSearchBurst {
+		if code, _, _ := getUserSearch(t, srv, "b"); code != http.StatusBadRequest {
+			t.Fatalf("request %d: status %d, want 400", i, code)
+		}
+	}
+	code, hdr, _ := getUserSearch(t, srv, "bob@example")
+	if code != http.StatusTooManyRequests {
+		t.Fatalf("request past the burst: status %d, want 429", code)
+	}
+	if hdr.Get("Retry-After") == "" {
+		t.Error("429 without Retry-After")
+	}
+}
+
+func TestSearchUsersRefusesDelegatedCalls(t *testing.T) {
+	mgr, _, _ := newTestManager(t, searchTestUser("user-bob", "bob@example.com", "Bob"))
+	h := NewHandler(mgr)
+	r := mux.NewRouter()
+	sub := r.PathPrefix("/api").Subrouter()
+	sub.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			ctx := tenant.WithContext(req.Context(), tenant.TenantContext{User: "user-alice"})
+			ctx = tenant.WithDelegatedCall(ctx, tenant.DelegatedCall{User: "user-alice", Provider: "app-studio"})
+			next.ServeHTTP(w, req.WithContext(ctx))
+		})
+	})
+	h.RegisterUserOnly(sub)
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
+	if code, _, _ := getUserSearch(t, srv, "bob@example"); code != http.StatusForbidden {
+		t.Fatalf("delegated call: status %d, want 403", code)
+	}
+}

pkg/hub/restapi/users.go
@@ -18,13 +18,105 @@ package restapi
 
 import (
 	"net/http"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
+	"unicode/utf8"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	tenancyv1alpha1 "github.com/faroshq/faros/apis/tenancy/v1alpha1"
+	"github.com/faroshq/faros/pkg/hub/tenant"
+	"github.com/faroshq/faros/pkg/server/proxy"
 )
+
+// User search lets any signed-in person discover other accounts' emails, so
+// it is deliberately narrow: a query must be a real prefix, only a handful of
+// suggestions come back, and each caller gets a small token bucket. With the
+// portal's debounce and client-side narrowing, typing one address costs one or
+// two requests; walking the user directory costs minutes per few names.
+const (
+	// UserSearchMinQuery is the shortest query answered, in characters.
+	UserSearchMinQuery = 5
+	// userSearchMaxQuery bounds the query to a plausible email length.
+	userSearchMaxQuery = 254
+	// UserSearchMaxResults caps the suggestions per query.
+	UserSearchMaxResults = 5
+	// userSearchBurst requests are admitted at once per caller, then one
+	// more per userSearchRefill.
+	userSearchBurst  = 10
+	userSearchRefill = 6 * time.Second
+)
+
+// newUserSearchLimiter returns the per-caller bucket for GET /api/users/search.
+// It is keyed on the caller's User name, not a client address: the endpoint
+// only serves signed-in people, and one person behind many addresses is still
+// one person. Buckets are per hub replica.
+func newUserSearchLimiter() *proxy.IPRateLimiter {
+	return proxy.NewIPRateLimiter(userSearchRefill, userSearchBurst)
+}
+
+// UserSuggestion is one GET /api/users/search result: enough to recognise the
+// person and add them, nothing more.
+type UserSuggestion struct {
+	// User is the User CR name; the membership endpoints accept it.
+	User        string `json:"user"`
+	Email       string `json:"email"`
+	DisplayName string `json:"displayName,omitempty"`
+}
+
+// searchUsers suggests accounts whose email or display name starts with the
+// query (case-insensitive), for the add-member box. Accounts without an email
+// (static tokens) are never suggested: they share their member ID directly,
+// and every one of their display names starts with the same "faros:static:".
+// Accounts being deleted and the caller themselves are left out too.
+func (h *Handler) searchUsers(w http.ResponseWriter, r *http.Request) {
+	user, ok := h.requireUser(w, r)
+	if !ok {
+		return
+	}
+	// People only: a provider acting with a delegated token has no business
+	// browsing the user directory.
+	if _, delegated := tenant.DelegatedCallFrom(r.Context()); delegated {
+		writeStatus(w, http.StatusForbidden, "Forbidden", "user search is not available to providers")
+		return
+	}
+	// Charge the bucket before validating, so malformed probes cost the same.
+	if !h.userSearch.Allow(user) {
+		w.Header().Set("Retry-After", strconv.Itoa(int(userSearchRefill/time.Second)))
+		writeStatus(w, http.StatusTooManyRequests, "TooManyRequests", "too many user searches; wait a few seconds")
+		return
+	}
+	q := strings.ToLower(strings.TrimSpace(r.URL.Query().Get("q")))
+	if n := utf8.RuneCountInString(q); n < UserSearchMinQuery || n > userSearchMaxQuery {
+		writeError(w, newValidationError("q must be "+strconv.Itoa(UserSearchMinQuery)+" to "+strconv.Itoa(userSearchMaxQuery)+" characters"))
+		return
+	}
+	list, err := h.mgr.client.Users().List(r.Context(), metav1.ListOptions{})
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	out := make([]UserSuggestion, 0, UserSearchMaxResults)
+	for i := range list.Items {
+		u := &list.Items[i]
+		if u.Name == user || u.Spec.Email == "" || u.Status.DeletionRequestedAt != nil {
+			continue
+		}
+		if !strings.HasPrefix(strings.ToLower(u.Spec.Email), q) &&
+			!strings.HasPrefix(strings.ToLower(u.Spec.Name), q) {
+			continue
+		}
+		out = append(out, UserSuggestion{User: u.Name, Email: u.Spec.Email, DisplayName: u.Spec.Name})
+	}
+	sort.Slice(out, func(i, j int) bool { return strings.ToLower(out[i].Email) < strings.ToLower(out[j].Email) })
+	if len(out) > UserSearchMaxResults {
+		out = out[:UserSearchMaxResults]
+	}
+	writeJSON(w, http.StatusOK, ListResponse[UserSuggestion]{Items: out})
+}
 
 // SelfView is the caller's own identity, as GET /api/users/me returns it.
 type SelfView struct {

railgrid/railgrid

pkg/hub/restapi/restapi.go
@@ -56,6 +56,7 @@ import (
 	"github.com/railgrid/railgrid/pkg/hub/kcp"
 	"github.com/railgrid/railgrid/pkg/hub/providers"
 	"github.com/railgrid/railgrid/pkg/hub/tenant"
+	"github.com/railgrid/railgrid/pkg/server/proxy"
 )
 
 // WorkspaceOps is the slice of *kcp.Bootstrapper the REST handlers
@@ -264,10 +265,14 @@ func (m *Manager) WithOrgProviders(ops OrgProviderOps, creds ProviderCredentialM
 // /api/* endpoints across the two middlewares.
 type Handler struct {
 	mgr *Manager
+	// userSearch rate-limits GET /api/users/search per caller.
+	userSearch *proxy.IPRateLimiter
 }
 
 // NewHandler constructs a Handler.
-func NewHandler(mgr *Manager) *Handler { return &Handler{mgr: mgr} }
+func NewHandler(mgr *Manager) *Handler {
+	return &Handler{mgr: mgr, userSearch: newUserSearchLimiter()}
+}
 
 // RegisterUserOnly attaches the routes that only need the caller's
 // User identity (no active Org / Workspace yet). r is the subrouter
@@ -278,12 +283,14 @@ func NewHandler(mgr *Manager) *Handler { return &Handler{mgr: mgr} }
 //	GET    /api/orgs                       list orgs the caller is in
 //	POST   /api/orgs                       create a new Org
 //	GET    /api/users/me                   the caller's own identity
+//	GET    /api/users/search?q=            suggest users by email/name prefix (rate-limited)
 //	DELETE /api/users/me                   soft-delete self (O-8)
 //	POST   /api/users/me/undelete          undelete self (O-8)
 func (h *Handler) RegisterUserOnly(r *mux.Router) {
 	r.HandleFunc("/orgs", h.listOrgs).Methods(http.MethodGet)
 	r.HandleFunc("/orgs", h.createOrg).Methods(http.MethodPost)
 	r.HandleFunc("/users/me", h.getSelfUser).Methods(http.MethodGet)
+	r.HandleFunc("/users/search", h.searchUsers).Methods(http.MethodGet)
 	r.HandleFunc("/users/me", h.deleteSelfUser).Methods(http.MethodDelete)
 	r.HandleFunc("/users/me/undelete", h.undeleteSelfUser).Methods(http.MethodPost)
 }

pkg/hub/restapi/user_search_test.go
@@ -1,0 +1,178 @@
+/*
+Copyright 2026 The Railgrid Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package restapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+
+	"github.com/gorilla/mux"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	tenancyv1alpha1 "github.com/railgrid/railgrid/apis/tenancy/v1alpha1"
+	"github.com/railgrid/railgrid/pkg/hub/tenant"
+)
+
+func searchTestUser(name, email, displayName string) *tenancyv1alpha1.User {
+	return &tenancyv1alpha1.User{
+		ObjectMeta: metav1.ObjectMeta{Name: name},
+		Spec:       tenancyv1alpha1.UserSpec{Email: email, Name: displayName, RBACIdentity: "railgrid:" + email},
+	}
+}
+
+func getUserSearch(t *testing.T, srv *httptest.Server, q string) (int, http.Header, []UserSuggestion) {
+	t.Helper()
+	resp, err := http.Get(srv.URL + "/api/users/search?q=" + url.QueryEscape(q))
+	if err != nil {
+		t.Fatalf("GET /api/users/search: %v", err)
+	}
+	defer func() { _ = resp.Body.Close() }()
+	var body ListResponse[UserSuggestion]
+	if resp.StatusCode == http.StatusOK {
+		if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
+			t.Fatalf("decoding: %v", err)
+		}
+	}
+	return resp.StatusCode, resp.Header, body.Items
+}
+
+func TestSearchUsers(t *testing.T) {
+	deleting := searchTestUser("user-gone", "carol.gone@example.com", "Carol Gone")
+	now := metav1.Now()
+	deleting.Status.DeletionRequestedAt = &now
+	static := &tenancyv1alpha1.User{
+		ObjectMeta: metav1.ObjectMeta{Name: "static-user-47b9dce0e91570a1"},
+		Spec:       tenancyv1alpha1.UserSpec{Name: "railgrid:static:47b9dce0e91570a1", RBACIdentity: "railgrid:static:47b9dce0e91570a1"},
+	}
+	mgr, _, _ := newTestManager(t,
+		searchTestUser("user-alice", "alice@example.com", "Alice"),
+		searchTestUser("user-carol", "Carol.Smith@example.com", "Carol Smith"),
+		searchTestUser("user-carl", "carl@example.com", "Carolina Jones"),
+		deleting,
+		static,
+	)
+	srv := newTestServer(t, mgr, adminTC("user-alice", "", ""))
+	defer srv.Close()
+
+	for _, tc := range []struct {
+		name string
+		q    string
+		want []string
+	}{
+		// Case-insensitive email prefix; a soft-deleted account is left out.
+		{name: "email prefix", q: "CAROL.", want: []string{"user-carol"}},
+		// Email and display-name matches combine, sorted by email.
+		{name: "email or name prefix", q: "carol", want: []string{"user-carl", "user-carol"}},
+		// Display-name prefix matches too.
+		{name: "name prefix", q: "carolina", want: []string{"user-carl"}},
+		// The caller is never suggested to themselves.
+		{name: "self excluded", q: "alice@", want: nil},
+		// Static-token users have no email and are never suggested, even
+		// though their display names share this prefix.
+		{name: "static excluded", q: "railgrid:static:", want: nil},
+		// Prefix only, not substring.
+		{name: "no substring match", q: "example.com", want: nil},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			code, _, items := getUserSearch(t, srv, tc.q)
+			if code != http.StatusOK {
+				t.Fatalf("status %d, want 200", code)
+			}
+			var got []string
+			for _, s := range items {
+				got = append(got, s.User)
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("q=%q suggested %v, want %v", tc.q, got, tc.want)
+			}
+		})
+	}
+
+	if code, _, _ := getUserSearch(t, srv, "caro"); code != http.StatusBadRequest {
+		t.Errorf("4-character query: status %d, want 400", code)
+	}
+}
+
+func TestSearchUsersCapsResults(t *testing.T) {
+	var objs []runtime.Object
+	for i := 7; i >= 0; i-- {
+		objs = append(objs, searchTestUser(fmt.Sprintf("user-%d", i), fmt.Sprintf("teammate%d@example.com", i), ""))
+	}
+	mgr, _, _ := newTestManager(t, objs...)
+	srv := newTestServer(t, mgr, adminTC("someone", "", ""))
+	defer srv.Close()
+
+	code, _, items := getUserSearch(t, srv, "teammate")
+	if code != http.StatusOK {
+		t.Fatalf("status %d, want 200", code)
+	}
+	var got []string
+	for _, s := range items {
+		got = append(got, s.Email)
+	}
+	want := []string{"teammate0@example.com", "teammate1@example.com", "teammate2@example.com", "teammate3@example.com", "teammate4@example.com"}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %v, want the first %d by email", got, UserSearchMaxResults)
+	}
+}
+
+func TestSearchUsersRateLimited(t *testing.T) {
+	mgr, _, _ := newTestManager(t, searchTestUser("user-bob", "bob@example.com", "Bob"))
+	srv := newTestServer(t, mgr, adminTC("user-alice", "", ""))
+	defer srv.Close()
+
+	// Invalid queries are charged too, so probing costs the same.
+	for i := range userSearchBurst {
+		if code, _, _ := getUserSearch(t, srv, "b"); code != http.StatusBadRequest {
+			t.Fatalf("request %d: status %d, want 400", i, code)
+		}
+	}
+	code, hdr, _ := getUserSearch(t, srv, "bob@example")
+	if code != http.StatusTooManyRequests {
+		t.Fatalf("request past the burst: status %d, want 429", code)
+	}
+	if hdr.Get("Retry-After") == "" {
+		t.Error("429 without Retry-After")
+	}
+}
+
+func TestSearchUsersRefusesDelegatedCalls(t *testing.T) {
+	mgr, _, _ := newTestManager(t, searchTestUser("user-bob", "bob@example.com", "Bob"))
+	h := NewHandler(mgr)
+	r := mux.NewRouter()
+	sub := r.PathPrefix("/api").Subrouter()
+	sub.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			ctx := tenant.WithContext(req.Context(), tenant.TenantContext{User: "user-alice"})
+			ctx = tenant.WithDelegatedCall(ctx, tenant.DelegatedCall{User: "user-alice", Provider: "app-studio"})
+			next.ServeHTTP(w, req.WithContext(ctx))
+		})
+	})
+	h.RegisterUserOnly(sub)
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
+	if code, _, _ := getUserSearch(t, srv, "bob@example"); code != http.StatusForbidden {
+		t.Fatalf("delegated call: status %d, want 403", code)
+	}
+}

pkg/hub/restapi/users.go
@@ -18,13 +18,105 @@ package restapi
 
 import (
 	"net/http"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
+	"unicode/utf8"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	tenancyv1alpha1 "github.com/railgrid/railgrid/apis/tenancy/v1alpha1"
+	"github.com/railgrid/railgrid/pkg/hub/tenant"
+	"github.com/railgrid/railgrid/pkg/server/proxy"
 )
+
+// User search lets any signed-in person discover other accounts' emails, so
+// it is deliberately narrow: a query must be a real prefix, only a handful of
+// suggestions come back, and each caller gets a small token bucket. With the
+// portal's debounce and client-side narrowing, typing one address costs one or
+// two requests; walking the user directory costs minutes per few names.
+const (
+	// UserSearchMinQuery is the shortest query answered, in characters.
+	UserSearchMinQuery = 5
+	// userSearchMaxQuery bounds the query to a plausible email length.
+	userSearchMaxQuery = 254
+	// UserSearchMaxResults caps the suggestions per query.
+	UserSearchMaxResults = 5
+	// userSearchBurst requests are admitted at once per caller, then one
+	// more per userSearchRefill.
+	userSearchBurst  = 10
+	userSearchRefill = 6 * time.Second
+)
+
+// newUserSearchLimiter returns the per-caller bucket for GET /api/users/search.
+// It is keyed on the caller's User name, not a client address: the endpoint
+// only serves signed-in people, and one person behind many addresses is still
+// one person. Buckets are per hub replica.
+func newUserSearchLimiter() *proxy.IPRateLimiter {
+	return proxy.NewIPRateLimiter(userSearchRefill, userSearchBurst)
+}
+
+// UserSuggestion is one GET /api/users/search result: enough to recognise the
+// person and add them, nothing more.
+type UserSuggestion struct {
+	// User is the User CR name; the membership endpoints accept it.
+	User        string `json:"user"`
+	Email       string `json:"email"`
+	DisplayName string `json:"displayName,omitempty"`
+}
+
+// searchUsers suggests accounts whose email or display name starts with the
+// query (case-insensitive), for the add-member box. Accounts without an email
+// (static tokens) are never suggested: they share their member ID directly,
+// and every one of their display names starts with the same "railgrid:static:".
+// Accounts being deleted and the caller themselves are left out too.
+func (h *Handler) searchUsers(w http.ResponseWriter, r *http.Request) {
+	user, ok := h.requireUser(w, r)
+	if !ok {
+		return
+	}
+	// People only: a provider acting with a delegated token has no business
+	// browsing the user directory.
+	if _, delegated := tenant.DelegatedCallFrom(r.Context()); delegated {
+		writeStatus(w, http.StatusForbidden, "Forbidden", "user search is not available to providers")
+		return
+	}
+	// Charge the bucket before validating, so malformed probes cost the same.
+	if !h.userSearch.Allow(user) {
+		w.Header().Set("Retry-After", strconv.Itoa(int(userSearchRefill/time.Second)))
+		writeStatus(w, http.StatusTooManyRequests, "TooManyRequests", "too many user searches; wait a few seconds")
+		return
+	}
+	q := strings.ToLower(strings.TrimSpace(r.URL.Query().Get("q")))
+	if n := utf8.RuneCountInString(q); n < UserSearchMinQuery || n > userSearchMaxQuery {
+		writeError(w, newValidationError("q must be "+strconv.Itoa(UserSearchMinQuery)+" to "+strconv.Itoa(userSearchMaxQuery)+" characters"))
+		return
+	}
+	list, err := h.mgr.client.Users().List(r.Context(), metav1.ListOptions{})
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	out := make([]UserSuggestion, 0, UserSearchMaxResults)
+	for i := range list.Items {
+		u := &list.Items[i]
+		if u.Name == user || u.Spec.Email == "" || u.Status.DeletionRequestedAt != nil {
+			continue
+		}
+		if !strings.HasPrefix(strings.ToLower(u.Spec.Email), q) &&
+			!strings.HasPrefix(strings.ToLower(u.Spec.Name), q) {
+			continue
+		}
+		out = append(out, UserSuggestion{User: u.Name, Email: u.Spec.Email, DisplayName: u.Spec.Name})
+	}
+	sort.Slice(out, func(i, j int) bool { return strings.ToLower(out[i].Email) < strings.ToLower(out[j].Email) })
+	if len(out) > UserSearchMaxResults {
+		out = out[:UserSearchMaxResults]
+	}
+	writeJSON(w, http.StatusOK, ListResponse[UserSuggestion]{Items: out})
+}
 
 // SelfView is the caller's own identity, as GET /api/users/me returns it.
 type SelfView struct {